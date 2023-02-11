1c4e7cb2fc78f222766bcb2ff6b5585e3dc63793
Hownda <117111277+Hownda@users.noreply.github.com>
2023-02-11 23:18:53 +0100

    Fixed bugs in formations

--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -2,19 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
+using System.Linq;
 
+[RequireComponent(typeof(SelectedUnitDictionary))]
 public class UnitMovement : MonoBehaviour
 {
-    private NavMeshAgent unitNavMeshAgent;
-    public SelectedUnitDictionary unitDictionary;
+    private SelectedUnitDictionary unitDictionary;
+    private List<Vector3> formationPositions = new List<Vector3>();
 
-    void Start()
+    public float spread;
+
+    private void Start()
     {
-        unitDictionary = GameObject.FindGameObjectWithTag("EventSystem").GetComponent<SelectedUnitDictionary>();
-        unitNavMeshAgent = GetComponent<NavMeshAgent>();
+        unitDictionary = GetComponent<SelectedUnitDictionary>();
     }
 
-    void Update()
+    private void Update()
     {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 50000))
+            {
+                if (unitDictionary.selectedTable.Count > 1)
+                {
+                    CalculateFormation(hit.point);
+                }
+                else
+                {
+                    foreach (KeyValuePair<int, GameObject> unit in unitDictionary.selectedTable)
+                    {
+                        unit.Value.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+                    }
+                }
+            }
+        }
+    }
+    public void CalculateFormation(Vector3 destination)
+    {
+        if (unitDictionary.selectedTable.Count % 2 == 0)
+        {
+            int formationWidth = unitDictionary.selectedTable.Count / 2;
+            int formationDepth = unitDictionary.selectedTable.Count / 2;
+
+            Vector3 middleOffset = new Vector3(formationWidth * 0.5f, 0, formationDepth * 0.5f);
+
+            for (int x = 0; x < formationWidth; x++)
+            {
+                for (int z = 0; z < formationDepth; z++)
+                {
+                    Vector3 pos = new Vector3(x, 0, z);
+                    pos -= middleOffset;
+                    pos *= spread;
+                    formationPositions.Add(pos + destination);
+                }
+            }
+            SendFormationData();
+            formationPositions.Clear();
+        }
+    }
+
+    private void SendFormationData()
+    {
+        ShuffleList();
+        int i = 0;
+        foreach (KeyValuePair<int, GameObject> unit in unitDictionary.selectedTable)
+        {
+            unit.Value.GetComponent<NavMeshAgent>().SetDestination(formationPositions[i]);
+            i++;
+            if (i > formationPositions.Count)
+            {
+                i = 0;
+            }
+        }
+    }
+
+    private void ShuffleList()
+    {
+        System.Random rand = new System.Random();
+        unitDictionary.selectedTable = unitDictionary.selectedTable.OrderBy(x => rand.Next()).ToDictionary(item => item.Key, item => item.Value);
+
     }
 }
--- Assets/Scripts/Units/UnitSelection/SelectedUnitDictionary.cs
+++ Assets/Scripts/Units/UnitSelection/SelectedUnitDictionary.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
 
 public class SelectedUnitDictionary : MonoBehaviour
 {
     public Dictionary<int, GameObject> selectedTable = new Dictionary<int, GameObject>();
-    private List<Vector3> formationPositions = new List<Vector3>();
-    public float spread;
 
     public void AddSelected(GameObject addedObject)
     {
@@ -39,73 +39,5 @@ public class SelectedUnitDictionary : MonoBehaviour
             }
         }
         selectedTable.Clear();
-    }
-
-    /*The following code checks if there are more than one unit selected if the player presses the right mouse button.
-    If there are more than one, the script will calculate a formation for each of the units in the dictionary.
-    */
-    private void Update()
-    {
-        if (Input.GetMouseButton(1))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-              
-            if (Physics.Raycast(ray, out hit, 50000))
-            {
-                if (selectedTable.Count > 1)
-                {
-                    CalculateFormation(hit.point);
-                }
-                else
-                {
-                    foreach (KeyValuePair<int, GameObject> unit in selectedTable)
-                    { 
-                        unit.Value.GetComponent<NavMeshAgent>().SetDestination(hit.point);
-                    }
-                }
-            }           
-        }
-    }
-    public void CalculateFormation(Vector3 destination)
-    {
-        if (selectedTable.Count % 2 == 0)
-        {
-            int formationWidth = selectedTable.Count / 2;
-            int formationDepth = selectedTable.Count / 2;
-
-            Vector3 middleOffset = new Vector3(formationWidth * 0.5f, 0, formationDepth * 0.5f);
-
-            for (int x = 0; x < formationWidth; x++)
-            {
-                for (int z = 0; z < formationDepth; z++)
-                {
-                    Vector3 pos = new Vector3(x, 0, z);
-                    pos += middleOffset;
-                    pos *= spread;
-                    formationPositions.Add(pos + destination);
-                }
-            }
-            SendFormationData();
-            formationPositions.Clear();
-        }
-        
-    }
-
-    private void SendFormationData()
-    {
-        int i = 0;
-        Debug.Log(formationPositions.Count);
-        foreach (KeyValuePair<int, GameObject> unit in selectedTable)
-        {
-            Debug.Log(formationPositions[i]);
-            Debug.Log(i + ". iteration");
-            unit.Value.GetComponent<NavMeshAgent>().SetDestination(formationPositions[i]);
-            i++;
-            if (i > formationPositions.Count)
-            {
-                i = 0;
-            }
-        }
     }
 }
